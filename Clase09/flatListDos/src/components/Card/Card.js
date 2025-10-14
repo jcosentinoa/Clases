2eca7af1262e163270f0410f44b8fd3fa79dbf6c
jcosentinoa <cosentinoj@udesa.edu.ar>
2025-10-14 11:41:52 -0300

    styles ejercicio 2

--- Clase09/flatListDos/src/components/Card/Card.js
+++ Clase09/flatListDos/src/components/Card/Card.js
@@ -3,11 +3,11 @@ import { Image, StyleSheet, Text, View } from "react-native";
 
 function Card(props) {
     return (
-        <View>
-            <Text>title: {props.title}</Text>
-            <Text>price: {props.price}</Text>
-            <Text>description: {props.description}</Text>
-            <Text>category: {props.category}</Text>
+        <View style={styles.card}>
+            <Text style={styles.title}>{props.title}</Text>
+            <Text>Price: {props.price}</Text>
+            <Text>Description: {props.description}</Text>
+            <Text>Category: {props.category}</Text>
             <Image style={styles.image} 
               source={{uri: props.image}}
               resizeMode='contain'/>
@@ -17,8 +17,20 @@ function Card(props) {
 }
 const styles= StyleSheet.create({
     image: {
-        height: 300,
-        width: 400
+        height: 100,
+        width: 200,
+        marginTop: 10,
+        alignSelf: "center"
+      },
+      card: {
+        backgroundColor: "#ccc",
+        borderRadius: 4,
+        padding: 14,
+        margin: 10
+      },
+      title: {
+        fontWeight: "bold",
+        alignSelf: "center",
       }
 })
 export default Card;
